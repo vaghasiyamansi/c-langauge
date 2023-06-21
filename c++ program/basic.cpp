6db9a284ce9b6aa63412cea20e9067af5633ed02
mansi <vaghasiyamaahigmail.com>
2023-06-21 09:18:43 +0530

    Change

---
+++ c++ program/basic.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+       int a,b;
+       cout<<"enter a:";
+       cin>>a;
+       cout<<"enter b:";
+       cin>>b;
+       int t=a*b;
+       cout<<"product is:"<<t;
+}
